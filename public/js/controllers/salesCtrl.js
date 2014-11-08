e2107ba7dd5c05fcb21fc9a58a425fe8d5669754
Joe Parrinello <joe@hardorange.org>
2014-11-08 16:22:29 -0500

    Formatted Form to look pretty, with all data.

--- public/js/controllers/salesCtrl.js
+++ public/js/controllers/salesCtrl.js
@@ -5,16 +5,11 @@ angular.module('SalesCtrl', []).controller('salesController',['$sce','$scope',fu
     "use strict";
     $scope.customer = {};
     $scope.customers = [
-        { lastName:"Savage", firstName: 'Adam',     email: 'adam@email.com',      age: 12, country: 'United States' },
-        { lastName:"Sanchez", firstName: 'Amalie',    email: 'amalie@email.com',    age: 12, country: 'Argentina' },
-        { lastName:"Adams", firstName: 'Estefanía', email: 'estefania@email.com', age: 21, country: 'Argentina' },
-        { lastName:"Emilio", firstName: 'Adrian',    email: 'adrian@email.com',    age: 21, country: 'Ecuador' },
-        { lastName:"Esteban", firstName: 'Wladimir',  email: 'wladimir@email.com',  age: 30, country: 'Ecuador' },
-        { lastName:"Frenchy", firstName: 'Samantha',  email: 'samantha@email.com',  age: 30, country: 'United States' },
-        { lastName:"Nicole", firstName: 'Nicole',    email: 'nicole@email.com',    age: 43, country: 'Colombia' },
-        { lastName:"Reddit", firstName: 'Natasha',   email: 'natasha@email.com',   age: 54, country: 'Ecuador' },
-        { lastName:"Find Me", firstName: 'Michael',   email: 'michael@email.com',   age: 15, country: 'Colombia' },
-        { lastName:"O'Connor", firstName: 'Nicolás',   email: 'nicolas@email.com',    age: 43, country: 'Colombia' }
+        { customerId:"000000001", lastName:"Savage", firstName: 'Adam',     email: 'adam@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Dayton", state:"NJ", zipCode:"08810"},
+        { customerId:"000000002", lastName:"Savage", firstName: 'Adam',    email: 'adam@gmail.com', addressLine1:"123 Any Street", addressLine2:"", city:"Princeton", state:"NJ", zipCode:"08540"},
+        { customerId:"000000003", lastName:"Adams", firstName: 'Estefanía', email: 'estefania@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Doylestown", state:"PA", zipCode:"00224"},
+        { customerId:"000000004", lastName:"Emilio", firstName: 'Adrian',    email: 'adrian@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Philadelphia", state:"PA", zipCode:"00223"},
+        { customerId:"000000005", lastName:"Esteban", firstName: 'Wladimir',  email: 'wladimir@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Newark", state:"NJ", zipCode:"07103"}
     ];
 
     $scope.trustAsHtml = function(value) {
