3c62a32bd201ba0f25182330f0dc746ec558cba3
Joe Parrinello <joe@hardorange.org>
2014-11-08 16:44:47 -0500

    New Fields, and support with date formatting

--- public/js/controllers/salesCtrl.js
+++ public/js/controllers/salesCtrl.js
@@ -5,11 +5,11 @@ angular.module('SalesCtrl', []).controller('salesController',['$sce','$scope',fu
     "use strict";
     $scope.customer = {};
     $scope.customers = [
-        { customerId:"000000001", lastName:"Savage", firstName: 'Adam',     email: 'adam@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Dayton", state:"NJ", zipCode:"08810"},
-        { customerId:"000000002", lastName:"Savage", firstName: 'Adam',    email: 'adam@gmail.com', addressLine1:"123 Any Street", addressLine2:"", city:"Princeton", state:"NJ", zipCode:"08540"},
-        { customerId:"000000003", lastName:"Adams", firstName: 'Estefanía', email: 'estefania@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Doylestown", state:"PA", zipCode:"00224"},
-        { customerId:"000000004", lastName:"Emilio", firstName: 'Adrian',    email: 'adrian@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Philadelphia", state:"PA", zipCode:"00223"},
-        { customerId:"000000005", lastName:"Esteban", firstName: 'Wladimir',  email: 'wladimir@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Newark", state:"NJ", zipCode:"07103"}
+        { customerId:"000000001", lastName:"Savage", firstName: 'Adam',     email: 'adam@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Dayton", state:"NJ", zipCode:"08810", dob:new Date("1980-01-31"), phone:"9088675309"},
+        { customerId:"000000002", lastName:"Savage", firstName: 'Adam',    email: 'adam@gmail.com', addressLine1:"123 Any Street", addressLine2:"", city:"Princeton", state:"NJ", zipCode:"08540", dob:new Date("1990-01-31"), phone:"9088675309"},
+        { customerId:"000000003", lastName:"Adams", firstName: 'Estefanía', email: 'estefania@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Doylestown", state:"PA", zipCode:"00224", dob:new Date("1970-01-31"), phone:"9088675309"},
+        { customerId:"000000004", lastName:"Emilio", firstName: 'Adrian',    email: 'adrian@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Philadelphia", state:"PA", zipCode:"00223", dob:new Date("1980-01-20"), phone:"9088675309"},
+        { customerId:"000000005", lastName:"Esteban", firstName: 'Wladimir',  email: 'wladimir@email.com', addressLine1:"123 Any Street", addressLine2:"", city:"Newark", state:"NJ", zipCode:"07103", dob:new Date("1980-03-31"), phone:"9088675309"}
     ];
 
     $scope.trustAsHtml = function(value) {
