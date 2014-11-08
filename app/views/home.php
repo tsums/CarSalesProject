8a86759e85aff3f2e311bf3de4f29332d271db07
Joe Parrinello <joe@hardorange.org>
2014-11-08 01:40:50 -0500

    Hello World, with all the Resources.

---
+++ app/views/home.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel and Angular Comment System</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css"> <!-- load bootstrap via cdn -->
+    <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+
+    <!-- JS -->
+    <script src="bower_components/angular/angular.min.js"></script> <!-- load angular -->
+    <script src="bower_components/angular-ui-select/dist/select.min.js"></script>
+
+    <!-- ANGULAR -->
+    <!-- all angular resources will be loaded from the /public folder -->
+    <script src="js/app.js"></script> <!-- load our application -->
+
+</head>
+<body>
+Hello World!
+</body>
+</html>
