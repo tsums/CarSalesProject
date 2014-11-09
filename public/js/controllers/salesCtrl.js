b0b2c218cfe66ecaa9e0ee14afc42dab94336b3b
Joe Parrinello <joe@hardorange.org>
2014-11-09 00:21:17 -0500

    Form Validation, Able to Make new Customer

--- public/js/controllers/salesCtrl.js
+++ public/js/controllers/salesCtrl.js
@@ -3,6 +3,7 @@
  */
 angular.module('SalesCtrl', []).controller('salesController',['$location','$scope','$http','customerSaleService',function($location, $scope, $http, customerSaleService){
     "use strict";
+    $scope.submitting = false;
     $scope.customer = {};
     $scope.customers = [];
     $http.get('/api/customers').
@@ -25,10 +26,31 @@ angular.module('SalesCtrl', []).controller('salesController',['$location','$scop
         else return "Save and ";
     };
 
+    var validateCreate = function(jsonValidate){
+        if(jsonValidate.name_last&&jsonValidate.name_first&&jsonValidate.address_1&&jsonValidate.city&&(jsonValidate.state).length==2&&(jsonValidate.zip).length==5&&jsonValidate.phone&&(jsonValidate.birthDate.toISOString().substring(0,10)).length==10&&jsonValidate.email){
+            return true;
+        } else{
+            console.log(jsonValidate);
+            return false;
+        }
+    };
+
     $scope.submitAndContinue = function(){
-        if($scope.customer.selected){
+        $scope.submitting = true;
+        if($scope.customer.selected) {
             customerSaleService.saveCustomerIdNumber($scope.customer.selected.id);
             $location.path('/carsale');
+        } else if ($scope.customer.create && validateCreate($scope.customer.create)){
+            $scope.customer.create.birthDate.toISOString().substring(0,10);
+            $http.post('/api/customers',$scope.customer.create).success(function(data){
+                customerSaleService.saveCustomerIdNumber(data.id);
+                $location.path('/carsale');
+            }).error(function(data){
+                console.log(data);
+                $scope.submitting = false;
+            });
+        } else {
+            $scope.submitting = false;
         }
     }
 
