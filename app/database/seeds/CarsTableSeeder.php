51b4e80f6bc8d016c6f8ddb0b1ba310b9f07b733
tsums <trevor@trevorsummerfield.com>
2014-11-30 20:29:46 -0500

    Further increased CarsTableSeeder so that models can only have one make.

--- app/database/seeds/CarsTableSeeder.php
+++ app/database/seeds/CarsTableSeeder.php
@@ -5,23 +5,27 @@ use Faker\Factory as Faker;
 
 class CarsTableSeeder extends Seeder {
 
-	public function run()
+    private $makes  = ['Ford', 'Honda', 'Toyota', 'Chevrolet', 'BMW', 'Oldsmobile', 'Saturn', 'Subaru', 'Dodge', 'Jeep', 'Chrysler', 'Mercedes', 'Tesla', 'Kia', 'Lexus', 'Lincoln', 'Acura', 'GMC'];
+    private $models = ['Excavator', 'Terminator', 'Cylon', 'Autobot', 'Mercenary', 'Troubadour', 'Cappuccino', 'Enforcer', 'Cosmonaut', 'Plumber', 'Impersonator', 'Relatiator', 'Bludgeoner', 'Pragmatizer', 'Ritualizer', 'Alchemist', 'Partitioner', 'Mover', 'Scribbler', 'Vortex', 'Behemoth', 'Colossus', 'Cerberus', 'Olympus', 'Macchiato', 'Miner', 'Jackrabbit', 'Rumbler', 'Destroyer', 'Corsair'];
+
+
+    public function run()
 	{
         DB::table('cars')->delete();
 		$faker = Faker::create();
 
         //makes fake cars, woo!
 
-        $makes = ['Ford', 'Honda', 'Toyota', 'Chevrolet', 'BMW', 'Oldsmobile', 'Saturn', 'Subaru', 'Dodge', 'Jeep', 'Chrysler', 'Mercedes', 'Tesla', 'Kia', 'Lexus', 'Lincoln', 'Acura', 'GMC'];
-        $models = ['Excavator', 'Terminator', 'Cylon', 'Autobot', 'Mercenary', 'Troubadour', 'Cappuccino', 'Enforcer', 'Cosmonaut', 'Plumber', 'Impersonator', 'Relatiator', 'Bludgeoner', 'Pragmatizer', 'Ritualizer', 'Alchemist', 'Partitioner', 'Mover', 'Scribbler', 'Vortex', 'Behemoth', 'Colossus', 'Cerberus', 'Olympus', 'Macchiato', 'Miner', 'Jackrabbit', 'Rumbler', 'Destroyer', 'Corsair'];
+        $cars    = $this->generateCars();
 
 		foreach(range(1, 100) as $index)
 		{
             $cost = $faker->numberBetween(12500, 27000);
+            $car = $cars[array_rand($cars)];
 			Car::create([
                 'VIN' => substr(strtoupper($faker->md5),16),
-                'make' => $makes[array_rand($makes)],
-                'model' => $models[array_rand($models)],
+                'make' => $car['make'],
+                'model' => $car['model'],
                 'year' => $faker->year,
                 'color' => $faker->colorName,
                 'msrp' => $cost * 1.15,
@@ -29,5 +33,25 @@ class CarsTableSeeder extends Seeder {
 			]);
 		}
 	}
+
+    public function generateCars()
+    {
+        shuffle($this->models);
+        shuffle($this->makes);
+        $cars = [];
+        foreach ($this->makes as $make) {
+
+            foreach (range(0, 2) as $index) {
+                $model = array_pop($this->models);
+                echo($model);
+                $cars[] = ['make' => $make, 'model' => $model];
+            }
+            if (count($this->models) == 0) {
+                break;
+            }
+
+        }
+        return $cars;
+    }
 
 }
