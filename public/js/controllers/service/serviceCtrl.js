fb3995f9fd261282ff0038cf27866b8c402a0312
Joe Parrinello <joe@hardorange.org>
2014-12-03 00:59:54 -0500

    Working on Service Types still.

--- public/js/controllers/service/serviceCtrl.js
+++ public/js/controllers/service/serviceCtrl.js
@@ -32,7 +32,7 @@ angular.module('ServiceCtrl', []).controller('serviceController',['$location','$
             error(function(data) {
                 console.log(data);
             });
-    }
+    };
 
     $scope.isValidCheckIn = function(){
         return $scope.appointment.arrived != null;
@@ -49,7 +49,44 @@ angular.module('ServiceCtrl', []).controller('serviceController',['$location','$
             error(function(data) {
                 console.log(data);
             });
-    }
+    };
+
+    $scope.servicesPerformed = [{}];
+
+    $scope.serviceTypes=[];
+
+    $http.get('/api/static/service-types').
+        success(function(data) {
+            console.log(data);
+            $scope.serviceTypes=data;
+        }).
+        error(function(data) {
+            console.log(data);
+        });
+
+    $scope.isValidInput = function(){
+        return $scope.appointment.selected!=undefined;
+    };
+
+    $scope.addServiceType = function(){
+        $scope.servicesPerformed.push({});
+    };
+
+    $scope.postServiceTypes = function(){
+        $scope.postServiceTypes = {};
+        console.log($scope.appointment);
+        angular.forEach($scope.servicesPerformed, function(value, key){
+            $scope.postServiceTypes[value.selected.id] = value.selected.suggested_price;
+        });
+        $http.post('/api/appointments/'+$scope.appointment.fetched.id+"/actions",$scope.postServiceTypes).
+            success(function(data){
+                console.log("posted service types");
+                console.log(data);
+            }).
+            error(function(data){
+                console.log(data);
+            });
+    };
 
     $scope.isValidCheckOut = function(){
         return $scope.appointment.departed != null;
