3144e14dec9ccaf314f0da952e206173794c6b18
Joe Parrinello <joe@hardorange.org>
2014-12-03 19:59:09 -0500

    New Seeders

--- app/database/seeds/ServiceTypesTableSeeder.php
+++ app/database/seeds/ServiceTypesTableSeeder.php
@@ -15,6 +15,16 @@ class ServiceTypesTableSeeder extends Seeder {
             'description' => 'Change Oil in Car',
             'suggested_price' => 49.95
         ]);
+        ServiceType::create([
+            'name' => 'Tire Rotation',
+            'description' => 'Rotate Tires',
+            'suggested_price' => 56.65
+        ]);
+        ServiceType::create([
+            'name' => 'Add Fluids',
+            'description' => 'Top all Fluids off.',
+            'suggested_price' => 60.00
+        ]);
     }
 
 } 
